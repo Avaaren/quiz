f276e17b489ae2ee0f80aae0b619efe1c36af85d
Avaaren <avaaren@mail.ru>
2020-03-03 19:58:26 +0300

    Some fix in password change views

--- quiz/account/urls.py
+++ quiz/account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 from . import views
 
@@ -13,12 +14,12 @@ urlpatterns = [
          auth_views.LoginView.as_view(template_name='account/logout.html'),
          name='logout'),
     path('registration/', views.registration, name='registration'),
-    path('password/change',
+    path('password_change/',
          auth_views.PasswordChangeView.as_view(
              template_name='account/password_change.html',
-             success_url='account:password_change_done'),
+             success_url=reverse_lazy('account:password_change_done')),
          name='password_change'),
-    path('password/change_done',
+    path('password_change/done/',
          auth_views.PasswordChangeDoneView.as_view(
              template_name='account/password_change_done.html'),
          name='password_change_done'),
