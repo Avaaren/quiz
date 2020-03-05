50a214b4f09e70da48822604db1bdb150997faa2
Avaaren <avaaren@mail.ru>
2020-03-05 15:27:39 +0300

    Added pagination for questions in quiz page

--- quiz/game/views.py
+++ quiz/game/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
     ListView,
     DetailView
 )
+from django.views.generic.list import MultipleObjectMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from management.models import (
@@ -22,8 +23,14 @@ class QuizListView(ListView, LoginRequiredMixin):
     template_name = 'game/quiz_list.html'
 
 
-class QuizDetailView(DetailView):
+class QuizDetailView(DetailView, MultipleObjectMixin):
     model = Quiz
     context_object_name = 'quiz'
     template_name = 'game/quiz_detail.html'
+    paginate_by = 1
 
+    def get_context_data(self, **kwargs):
+        object_list = self.object.questions.all()
+        context = super(QuizDetailView, self).get_context_data(object_list=object_list, **kwargs)
+        return context
+    
