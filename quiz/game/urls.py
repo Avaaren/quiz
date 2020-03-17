de15a23b585f2715aa3fd8b7e29e19615805dfe6
Avaaren <avaaren@mail.ru>
2020-03-17 16:46:12 +0300

    Added url for quiz_result

--- quiz/game/urls.py
+++ quiz/game/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('quiz/', views.QuizListView.as_view(), name='quiz_list'),
     path('quiz/<int:pk>/', views.quiz_detail, name='quiz_detail'),
+    path('quiz/result/<int:pk>', views.QuizResultView.as_view(), name='quiz_result'),
 ]
