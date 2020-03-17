233a08a51be6779348a1fb113fc9c08fe9e6772e
Avaaren <avaaren@mail.ru>
2020-03-17 16:48:08 +0300

    Added CBV for quiz result and edited quiz detail to show results of quiz

--- quiz/game/views.py
+++ quiz/game/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-import json
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import (
     ListView,
     DetailView
 )
-from django.views.generic.list import MultipleObjectMixin
+from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 
@@ -47,32 +45,65 @@ def quiz_detail(request, pk):
         is_finished=False
     )[0]
     question_queryset = current_quiz_object.quiz.questions.all()
+    total_questions = len(question_queryset)
+    current_question_id = current_quiz_object.questions_passed + 1
+
+    if current_question_id > total_questions:
+        return redirect(quiz_object.get_absolute_result_url())
 
     if request.method == 'POST':
         response_data = {}
         # мы отправляем сюда ажакс пост запрос и туда кидаем атрибут экшн
         if request.POST.get('action') == 'post':
             # Получаем айди ответа переданный в ажакс и смотрим правильный ли ответ, если да, то...
-            current_question_id = current_quiz_object.questions_passed + 1
-            chosen_answer_id = request.POST.get('chosen_answer_id', None)
-            answer = current_quiz_object.quiz.questions.get(
-                pk=current_question_id).answers.get(pk=chosen_answer_id)
 
-            if answer.is_correct:
-                current_quiz_object.questions_passed += 1
-                current_quiz_object.total_score += 1
-                response_data['is_correct'] = True
-                response_data['current_question_id'] = current_question_id+1
-                current_quiz_object.save()
+            chosen_answer_id = request.POST.get('chosen_answer_id', None)
+            print(chosen_answer_id)
+            if chosen_answer_id:
+                answer = current_quiz_object.quiz.questions.get(
+                    pk=current_question_id).answers.get(pk=chosen_answer_id)
+
+                if answer.is_correct:
+                    current_quiz_object.questions_passed += 1
+                    current_quiz_object.total_score += 1
+                    response_data['is_correct'] = True
+                    response_data['current_question_id'] = current_question_id+1
+                    current_quiz_object.save()
+                else:
+                    current_quiz_object.questions_passed += 1
+                    response_data['is_correct'] = False
+                    response_data['current_question_id'] = current_question_id + 1
+                    current_quiz_object.save()
+                
             else:
-                current_quiz_object.questions_passed += 1
-                response_data['is_correct'] = False
-                response_data['current_question_id'] = current_question_id + 1
-                current_quiz_object.save()
+                response_data['is_correct'] = True
+                response_data['current_question_id'] = current_question_id
+                print(current_question_id)
             return JsonResponse(response_data)
+    else:
+        return render(request, 'game/quiz_detail.html', {
+            'current_quiz_object': current_quiz_object,
+            'question_queryset': question_queryset,
+            'current_question_id': current_question_id,
+        })
 
     return render(request, 'game/quiz_detail.html', {
         'current_quiz_object': current_quiz_object,
         'question_queryset': question_queryset,
     })
 
+# quiz result url view!
+
+
+class QuizResultView(DetailView):
+    model = CurrentQuiz
+    context_object_name = 'quiz_result'
+    template_name = 'game/quiz_result.html'
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        quiz_object = get_object_or_404(
+            CurrentQuiz, quiz__pk=pk, is_finished=False)
+        quiz_object.is_finished = True
+        quiz_object.save()
+        return quiz_object
